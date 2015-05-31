9558c3ee0cfb2df1f463794485ef11914c089325
eaton11 <sameaton11@gmail.com>
2015-05-30 19:19:23 -0600

    knucklebone version 1.0

--- test_demo/cats.php
+++
@@ -1,7 +0,0 @@
-<?php 
-		usleep(mt_rand(50000,1000000));
-		// sleep(mt_rand() / mt_getrandmax());
-		$catJson = file_get_contents("cats.json");
-		// $catJson = json_encode(file_get_contents("cats.json"));
-		echo $catJson;
-?>
