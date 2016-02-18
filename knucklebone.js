2e9fcee89fd414d025c523a90a31932cec6f4705
Sam Eaton <sameaton11@gmail.com>
2016-02-17 17:22:21 -0700

    adds default export for es2015

--- knucklebone.js
+++ knucklebone.js
@@ -1,8 +1,12 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-module.exports = function () {
+exports.default = function () {
 
 	/* Lodash-esque helpers
  */
